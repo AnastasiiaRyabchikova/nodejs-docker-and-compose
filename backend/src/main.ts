5f45f2243d96beacfa48b77b6b2080e0ae32064c
Анастасия Рябчикова <a.ryabchikova@postgrespro.ru>
2025-08-16 12:28:06 +0300

    backend/src/main.ts: update

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.enableCors({
+    origin: ['https://kpd.ryabchulya.nomorepartiessbs.ru'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // если работаешь с cookie / Authorization headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 
